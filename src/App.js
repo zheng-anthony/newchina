6f1b5c1604797c2a83b55b0ffc7ce596f731e5a8
Anthony Zheng <anthonyzheng124@gmail.com>
2025-05-11 02:08:59 -0400

    beginning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import logo from './Pictures/Inside New China.webp';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div className="top_tab">
+        <h1 className="title">New China</h1>
+      </div>
+
+    {/* <div>
+      <img src={logo} alt="New China Logo" className="image" />    
+      </div> */}
+
+    
+
+    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <h1>Phone Number</h1>
+      <p>813-837-6865</p>
+      <a
+        href="https://www.ubereats.com/store/new-china/ZSgDp7c1SGGl6clZoEVTSw?diningMode=DELIVERY&ps=1&sc=SEARCH_SUGGESTION"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Link to UberEats!
+      </a>
     </div>
+    </>
   );
 }
-
-export default App;
